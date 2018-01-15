3c8f02dc23f38b829885c6a24c925bd4b0f74bcb
json.zhuo <json.zhuo@gmail.com>
2018-01-15 21:12:22 +0800

    electron add

--- app.js
+++ app.js
@@ -1,34 +1,35 @@
 var express = require('express');
 var cheerio = require('cheerio');
 var superagent = require('superagent');
-var fs = require('fs');
+var bodyParser = require('body-parser');
+var fs = require("fs");
 
 var app = express();
 
 var cnode = 'https://cnodejs.org/';
 const file = '/content.txt';
 
-app.get('/', function(req, res, next){
+app.get('/', function (req, res, next) {
     superagent.get('https://cnodejs.org/')
-    .end(function(err, sres){
-        if(err){
-            return next(err);
-        }
+        .end(function (err, sres) {
+            if (err) {
+                return next(err);
+            }
 
-        var $ = cheerio.load(sres.text);
-        var items = [];
-        $('#topic_list .topic_title').each(function(idx, element){
-            var $element = $(element);
-            items.push({
-                title:$element.attr('title'),
-                href:$(element).attr('href')
-            });            
+            var $ = cheerio.load(sres.text);
+            var items = [];
+            $('#topic_list .topic_title').each(function (idx, element) {
+                var $element = $(element);
+                items.push({
+                    title: $element.attr('title'),
+                    href: $(element).attr('href')
+                });
+            });
+
+            res.send(items);
         });
-
-        res.send(items);
-    });    
 });
 
-app.listen(3000, function(){
+app.listen(3000, function () {
     console.log('app is listening at port 3000');
 })
